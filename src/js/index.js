36a909923a5607dcd7a1dd60f697758bd71b35e7
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-28 19:39:01 -0300

    mobile cards in reviews section is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 import styles from '../styles/index.scss';
+
+(function ($) {
+  const config = {
+    // centerMode: true, // maybe use on desktop
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+  };
+  $(document).ready(function () {
+    $('.slider-container').slick(config);
+  });
+})(jQuery);
