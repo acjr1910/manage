d57b755c25c03b922c244a8471a074cf9c574d24
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-30 16:42:23 -0300

    adding mobile nav features

--- src/js/components/Menu/index.js
+++ src/js/components/Menu/index.js
@@ -5,23 +5,72 @@ function Menu() {
 
   const menuElement = document.querySelector('.header__menu');
   const navElement = document.querySelector('.header__nav');
-  const menuShowClass = 'header__nav--show';
+  const bodyElement = document.querySelector('body');
   const menuInitialClass = 'header__nav';
+  const menuShowClass = 'header__nav--show';
 
   function addListener() {
-    menuElement.addEventListener('click', toggleState);
+    return menuElement.addEventListener('click', toggleState);
   }
 
   function toggleState() {
     Object.assign({}, state, (state.isOpen = !state.isOpen));
     handleMenu();
+    return;
+  }
+
+  function makeOverlayDiv() {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'overlay');
+    return div;
+  }
+
+  function appendOverlay(target) {
+    if (target) return target.prepend(makeOverlayDiv());
+  }
+
+  function removeOverlay(target) {
+    if (target) return target.remove();
+  }
+
+  function lockBody() {
+    return (bodyElement.style.overflow = 'hidden');
+  }
+
+  function unlockBody() {
+    return (bodyElement.style.overflow = 'initial');
+  }
+
+  function setCloseIcon() {
+    return (menuElement.src = 'images/icon-close.svg');
+  }
+
+  function setHamburgerIcon() {
+    return (menuElement.src = 'images/icon-hamburger.svg');
+  }
+
+  function showNav() {
+    navElement.classList = menuShowClass;
+    appendOverlay(bodyElement);
+    lockBody();
+    setCloseIcon();
+    return;
+  }
+
+  function hideNav() {
+    navElement.classList = menuInitialClass;
+    const overlayElement = document.querySelector('.overlay');
+    removeOverlay(overlayElement);
+    unlockBody();
+    setHamburgerIcon();
+    return;
   }
 
   function handleMenu() {
     if (state.isOpen) {
-      return (navElement.classList = menuShowClass);
+      return showNav();
     }
-    return (navElement.classList = menuInitialClass);
+    return hideNav();
   }
 
   return addListener();
