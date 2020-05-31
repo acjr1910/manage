a517dbdf1230d40ccd889a4c3569bad15c58f0f5
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-31 19:11:08 -0300

    centered before elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-import styles from '../styles/index.scss';
 import Menu from './components/Menu/index';
 import Slider from './components/Slider/index';
+
+import styles from '../styles/index.scss';
 
 (function () {
   window.addEventListener('DOMContentLoaded', (event) => {
